fd8607cc7809762cc4b5dddb6e32a97b05c14f60
63ramil63 <63ramil63@gmail.com>
2025-05-08 19:03:46 +0400

    Переделаны страницы login и register

--- src/main/java/com/example/simplesite/controller/AuthController.java
+++ src/main/java/com/example/simplesite/controller/AuthController.java
@@ -3,12 +3,14 @@ package com.example.simplesite.controller;
 import com.example.simplesite.model.User;
 import com.example.simplesite.service.impl.UserServiceImpl;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.core.Authentication;
 
 @Controller
 @AllArgsConstructor
@@ -16,8 +18,18 @@ public class AuthController {
 
     private final UserServiceImpl userService;
 
+    private void setHeaderButtons(Authentication authentication, Model model) {
+        if (authentication.isAuthenticated() && !(authentication instanceof AnonymousAuthenticationToken)) {
+            model.addAttribute("isAuth", true);
+        } else {
+            model.addAttribute("isAuth", false);
+        }
+    }
+
     @GetMapping("/registerForm")
     public String registerForm(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        setHeaderButtons(authentication, model);
         model.addAttribute("user", new User());
         return "register";
     }
@@ -33,6 +45,8 @@ public class AuthController {
 
     @GetMapping("/login")
     public String login(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        setHeaderButtons(authentication, model);
         model.addAttribute("user", new User());
         return "login";
     }
